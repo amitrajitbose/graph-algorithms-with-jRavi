23a1be77a3d3ebb6ed4b0b51393d1c3a1f7f53a1
Amitrajit Bose <amitrajitbose9@gmail.com>
2019-06-23 17:16:21 +0530

    Complete chapter 1

--- intro/graph.py
+++ intro/graph.py
@@ -172,21 +172,20 @@ class AdjacencyMatrixGraph(Graph):
 			for v in self.get_adjacent_vertices(i):
 				print(i, "-->", v)
 
+if __name__ == '__main__':
+	g = AdjacencySetGraph(4)
+	g.add_edge(0, 1)
+	g.add_edge(0, 2)
+	g.add_edge(2, 3)
 
-g = AdjacencySetGraph(4)
-g.add_edge(0, 1)
-g.add_edge(0, 2)
-g.add_edge(2, 3)
+	for i in range(4):
+	    print("Adjacent to: ", i, g.get_adjacent_vertices(i))
 
-for i in range(4):
-    print("Adjacent to: ", i, g.get_adjacent_vertices(i))
+	for i in range(4):
+	    print("Indegree: ", i, g.get_indegree(i))
 
-for i in range(4):
-    print("Indegree: ", i, g.get_indegree(i))
+	for i in range(4):
+	    for j in g.get_adjacent_vertices(i):
+	        print ("Edge weight: ", i, " ", j, " weight: ", g.get_edge_weight(i, j))
 
-for i in range(4):
-    for j in g.get_adjacent_vertices(i):
-        print ("Edge weight: ", i, " ", j, " weight: ", g.get_edge_weight(i, j))
-
-g.display()
-
+	g.display()
